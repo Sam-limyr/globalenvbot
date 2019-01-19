ac3b3436721d30c78da9ec2396250bba7f54204f
Sahilgat <38809578+Sahilgat@users.noreply.github.com>
2019-01-19 22:27:04 +0800

    Add files via upload

---
+++ working_version2.py
@@ -0,0 +1,85 @@
+import json, requests, time, urllib
+from GlobalEnvbotDatabasehelper import DBHelper
+
+db = DBHelper()
+TOKEN = "709567258:AAFMJ6l_MDC0Q0c21beBptlUAVVXpCWRFi4"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=1000"
+    if offset:
+        url += "&offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+def handle_updates(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            items = db.get_items(chat)
+            starting = ['Setting', 'Closing', 'Leaderboard']
+            if text == "/start":
+                startboard = build_keyboard(starting)
+                send_message("Hi! Welcome to GlobalEnvbot!", chat, startboard)
+            elif text == "Setting":
+                layer1 = ['Register', 'Own status', 'Toggle reminder']
+                layer1subboard = build_keyboard(layer1)
+                send_message("Pick an option!", chat, layer1subboard)
+                #add logic for suboptions
+            elif text == "Closing":
+                send_message("Whose window?", chat) #add options from database
+            elif text == "Leaderboard":
+                send_message("Welcome to the leaderboards!", chat) #add link to database
+            elif text == "/done":
+                send_message("Exiting bot", chat)
+        except KeyError as e:
+            print(e)
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+def build_keyboard(items):
+    keyboard = [[item] for item in items]
+    reply_markup = {"keyboard":keyboard, "one_time_keyboard": True}
+    return json.dumps(reply_markup)
+
+def send_message(text, chat_id, reply_markup=None):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    if reply_markup:
+        url += "&reply_markup={}".format(reply_markup)
+    get_url(url)
+
+def main():
+    db.setup()
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            handle_updates(updates)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
